4c052cb0fed9d0ed5b06556f3a4990cc9b288463
Darío Jiménez <jeffrey.ayala2040@gmail.com>
2021-06-19 18:23:58 -0400

    Print Hello Express in the app

--- myApp.js
+++ myApp.js
@@ -1,41 +1,12 @@
 var express = require('express');
 var app = express();
 
+// TODO 1: Imprimir Hello World en consola
 console.log('Hello World');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// TODO 2: Imprimir Hello Express en la aplicación
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+})
 
  module.exports = app;
