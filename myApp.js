141a96fdbb0842e94736ba6da2976317cd5d088c
Darío Jiménez <jeffrey.ayala2040@gmail.com>
2021-06-20 08:04:59 -0400

    Create my own middleware

--- myApp.js
+++ myApp.js
@@ -5,6 +5,13 @@ var app = express();
 // TODO 4: Agregar middleware express.static(), para cargar los estilos y embellecer el HTML
 app.use('/public', express.static(__dirname + '/public'));
 
+// TODO 7: Crear mi propio middleware
+ app.use(function middleware(req, res, next) {
+    const str = req.method + " " + req.path + " - " + req.ip;
+    console.log(str);
+    next();
+});
+
 // TODO 1: Imprimir Hello World en consola
 console.log('Hello World');
 
@@ -22,7 +29,8 @@ const path = __dirname + '/views/index.html';
 
 // TODO 5: Enviar petición de una solicitud json
 app.get('/json', (req, res) => {
-    // TODO 6: Crear un archivo de variables de entorno .env para configurar la aplicación
+    // TODO 6: Crear un archivo de variables de entorno .env para configurar la aplicación 
+    // ! [NO LOGRADO]
     let response;
     if(process.env.MESSAGE_STYLE === "uppercase") {
         response = "Hello Json".toUpperCase();
@@ -30,6 +38,6 @@ app.get('/json', (req, res) => {
         response = "Hello Json";
     }
     res.json({"message": response});
-})
+});
 
  module.exports = app;
